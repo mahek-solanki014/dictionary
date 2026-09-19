58a2dd0042f7882763437da132a09d02f3294870
unknown <maheksolanki140314@gmail.com>
2026-09-19 12:26:12 +0400

    country code

---
+++ country_code.py
@@ -0,0 +1,7 @@
+country_code = {'india': 783453756, 'USA': 3687956, 'MahekLand': 4857075}
+print("country code for india")
+print(country_code.get('india', 'not found'))
+
+# search dictionary for wakanda
+print('country code for Wakanda')
+print(country_code.get('Wakanda', 'not found'))
